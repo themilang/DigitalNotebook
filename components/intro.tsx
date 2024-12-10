b5a3248361f98dc3e6d2c1f4650015605377176b
themilang <ghimiremilan.gn@gmail.com>
2024-12-10 19:36:05 +0545

    form added

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ export default function Intro() {
     <section className='flex flex-col-reverse items-start gap-x-6 gap-y-4 pb-16 md:flex-row md:items-center'>
       <div className='mt-0 flex-1 md:mt-0'>
         <h1 className='title no-underline font-mono text-3xl md:text-3xl '>
-          Hey, I&#39;m Milan <span className='font-mono text-sm text-sky-600'>/mi/lən/</span>
+          Hey, I&#39;m Milan <span className='font-mono text-sm text-sky-600'>/muh·lan/</span>
         </h1>
         <p className='mt-2 text-sm font-light text-muted-foreground leading-relaxed'>
           Welcome to my digital notebook. I’m a Computer Engineer and Fullstack Developer from Nepal. Here, I share my latest projects, insights into ML & AI, and lessons learned throughout my journey. Actively sharing knowledge with the community.
