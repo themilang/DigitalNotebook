06068e6b295ede16f888bac917720da6fb8c76b7
themilang <ghimiremilan.gn@gmail.com>
2024-12-11 17:32:34 +0545

    adsense

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,13 @@ import RecentProjects from '@/components/recent-projects'
 import CustomTestimonials from '@/components/testimonials'
 
 export default function Home() {
+  <script
+  async
+  src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+  crossOrigin="anonymous"
+></script>
   return (
+    
     <section className='pb-20 pt-40'>
       <div className='container max-w-3xl'>
         <Intro />
