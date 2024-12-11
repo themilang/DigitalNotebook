33c79423c407a71dd0d7469dbd5324729110e8c8
themilang <ghimiremilan.gn@gmail.com>
2024-12-11 21:09:34 +0545

    button removed link expanded

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+const siteUrl = "https://www.milanghimire.info.np";
+
+module. exports = {
+   
+siteUrl,
+generateRobotsTxt: true, // Generate a robots.txt file
+exclude: [], // Exclude specific pages or routes (optional)
+changefreq: 'daily', // Change frequency of pages (optional)
+priority: 0.8, // Default priority of pages (optional)
+};
