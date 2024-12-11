d52c249f13fd470400123b587fecfcf59b53a22a
themilang <ghimiremilan.gn@gmail.com>
2024-12-11 22:41:07 +0545

    robots updated

--- next-sitemap.js
+++ next-sitemap.js
@@ -8,13 +8,4 @@ exclude: [], // Exclude specific pages or routes (optional)
 changefreq: 'daily', // Change frequency of pages (optional)
 priority: 0.8, // Default priority of pages (optional)
 
-siteUrl: 'https://www.milanghimire.info.np', // Replace with your site URL
-  generateRobotsTxt: true, // Generate robots.txt file
-  additionalPaths: async (config) => [
-    {
-      loc: '/posts', // The URL path to include
-      changefreq: 'daily', // Optional: Change frequency
-      priority: 0.9, // Optional: Priority for this path
-    },
-  ],
 };
